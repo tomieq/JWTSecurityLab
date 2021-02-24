a73bb45119cda1556d9922057bc88ae170847d0e
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-02-24 18:14:26 +0100

    Implement Resources class

---
+++ Sources/JWTSecurityLab/webApp/Resource.swift
@@ -0,0 +1,32 @@
+//
+//  Resource.swift
+//  
+//
+//  Created by Tomasz Kucharski on 24/02/2021.
+//
+
+import Foundation
+
+class Resource {
+    private static let appResourcesDir = FileManager.default.currentDirectoryPath + "/AppResources/"
+    private static let publicResourcesPath = FileManager.default.currentDirectoryPath + "/PublicResources/"
+    
+    
+    static func getAppResource(relativePath: String) -> String {
+        let url = URL(fileURLWithPath: Resource.absolutePath(forAppResource: relativePath))
+        do {
+            return try String(contentsOf: url)
+        } catch {
+            Logger.error("Resource", "Error loading app resource from \(url.absoluteString)")
+        }
+        return ""
+    }
+    
+    static func absolutePath(forPublicResource relativePath: String) -> String {
+        return Resource.publicResourcesDir + relativePath.trimmingCharacters(in: CharacterSet(arrayLiteral: "/"))
+    }
+    
+    static func absolutePath(forAppResource relativePath: String) -> String {
+        return Resource.appResourcesDir + relativePath.trimmingCharacters(in: CharacterSet(arrayLiteral: "/"))
+    }
+}
