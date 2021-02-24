4de675a457065705d3922d3e08292b670ceefc00
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-02-24 16:18:31 +0100

    Add modified version of swifter http server

---
+++ Sources/JWTSecurityLab/httpServer/Files.swift
@@ -0,0 +1,55 @@
+//
+//  HttpHandlers+Files.swift
+//  Swifter
+//
+//  Copyright (c) 2014-2016 Damian Kołakowski. All rights reserved.
+//
+
+import Foundation
+
+public func shareFile(_ path: String) -> ((HttpRequest) -> HttpResponse) {
+    return { _ in
+        if let file = try? path.openForReading() {
+            return .raw(200, "OK", [:], { writer in
+                try? writer.write(file)
+                file.close()
+            })
+        }
+        return .notFound
+    }
+}
+
+public func shareFilesFromDirectory(_ directoryPath: String, defaults: [String] = ["index.html", "default.html"]) -> ((HttpRequest) -> HttpResponse) {
+    return { request in
+        guard let fileRelativePath = request.params.first else {
+            return .notFound
+        }
+        if fileRelativePath.value.isEmpty {
+            for path in defaults {
+                if let file = try? (directoryPath + String.pathSeparator + path).openForReading() {
+                    return .raw(200, "OK", [:], { writer in
+                        try? writer.write(file)
+                        file.close()
+                    })
+                }
+            }
+        }
+        let filePath = directoryPath + String.pathSeparator + fileRelativePath.value
+
+        if let file = try? filePath.openForReading() {
+            let mimeType = fileRelativePath.value.mimeType()
+            var responseHeader: [String: String] = ["Content-Type": mimeType]
+
+            if let attr = try? FileManager.default.attributesOfItem(atPath: filePath),
+                let fileSize = attr[FileAttributeKey.size] as? UInt64 {
+                responseHeader["Content-Length"] = String(fileSize)
+            }
+
+            return .raw(200, "OK", responseHeader, { writer in
+                try? writer.write(file)
+                file.close()
+            })
+        }
+        return .notFound
+    }
+}
