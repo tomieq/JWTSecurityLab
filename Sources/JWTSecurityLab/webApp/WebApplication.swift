61233153f312332de9689d0e02c26c0738ad3ecc
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-02-25 14:42:12 +0100

    Implement logic for lab 1

--- Sources/JWTSecurityLab/webApp/WebApplication.swift
+++ Sources/JWTSecurityLab/webApp/WebApplication.swift
@@ -13,13 +13,73 @@ class WebApplication {
     
     init(_ server: HttpServer) {
 
-        server["/"] = { request in
+        server.GET["/"] = { request in
             
-            let loginTemplate = Template(raw: Resource.getAppResource(relativePath: "templates/loginForm.html"))
             let template = Template(raw: Resource.getAppResource(relativePath: "templates/pageResponse.html"))
+            return template.asResponse()
+        }
+        
+        server["/lab1"] = { request in
+            
+            let cookieName = "lab1_cookie"
+            
+            let template = Template(raw: Resource.getAppResource(relativePath: "templates/pageResponse.html"))
+            let loginTemplate = Template(raw: Resource.getAppResource(relativePath: "templates/loginForm.html"))
+            template.assign(variables: ["title":"Lab 1: CVE-2020-15957"])
+            template.assign(variables: ["url":"/css/login-form.css"], toNest: "css")
+            
+            let instructions = Template(raw: Resource.getAppResource(relativePath: "templates/lab1Instructions.html"))
+            loginTemplate.assign(variables: ["instructions":instructions.output()])
+            
+            let headers = HttpHeaders()
+            var authorizedLogin: String?
+            
+            if let token = request.cookies[cookieName] {
+                do {
+                    authorizedLogin = try JWTdecode(token, algorithm: .hs256("secret".data(using: .utf8)!)).claims["user"] as? String
+                } catch {
+                    // simulated bahaviour of CVE-2020-15957
+                    do {
+                        authorizedLogin = try JWTdecode(token, algorithm: .none).claims["user"] as? String
+                    } catch {
+                        template.assign(variables: ["message":"Failed to decode JWT: \(error)"], toNest: "error")
+                    }
+                }
+            }
+            
+            let formData = request.parseUrlencodedForm()
+            if let login = (formData.first { $0.0 == "login" }.map { $0.1 }),
+                let password = (formData.first { $0.0 == "password" }.map { $0.1 }) {
+                    if ["jim", "admin"].contains(login) {
+                        if login == "jim", password == "12345" {
+                            let token = JWTencode(claims: ["user": login], algorithm: .hs256("secret".data(using: .utf8)!))
+                            headers.setCookie(name: cookieName, value: token + ";Max-Age=3000; HttpOnly, Secure")
+                            authorizedLogin = login
+                        } else {
+                            template.assign(variables: ["message":"Invalid password for user <b>\(login)</b>."], toNest: "error")
+                        }
+                    } else {
+                        template.assign(variables: ["message":"User <b>\(login)</b> does't exist."], toNest: "error")
+                    }
+            } else if let _ = (formData.first { $0.0 == "logout" }.map { $0.1 }) {
+                headers.unsetCookie(name: cookieName)
+                template.assign(variables: ["message":"Successfully signed out."], toNest: "success")
+                authorizedLogin = nil
+            }
+            
+            if let login = authorizedLogin, ["jim", "admin"].contains(login) {
+                loginTemplate.assign(variables: ["login":login], toNest: "authorized")
+                if login == "admin" {
+                    template.assign(variables: ["message":"Job well done! Contratulations. You have taken over admin's account!"], toNest: "success")
+                } else {
+                    template.assign(variables: ["message":"Successfully logged as \(login). Now your task is to breach the security and authenticate as admin."], toNest: "info")
+                }
+            } else {
+                loginTemplate.assign(variables: nil, toNest: "unauthorized")
+            }
+            
             template.assign(variables: ["body": loginTemplate.output()])
-            template.assign(variables: ["url" : "/css/login-form.css"], toNest: "css")
-            return template.asResponse(withHeaders: HttpHeaders().unsetCookie(name: "momo1"))
+            return template.asResponse(withHeaders: headers)
         }
         
         server.notFoundHandler = { request in
