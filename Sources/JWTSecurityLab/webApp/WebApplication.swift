5287152987df1d5455bcb6958570ee39263af011
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-02-26 07:40:11 +0100

    Implement lab 2 for CVE-2019-7644

--- Sources/JWTSecurityLab/webApp/WebApplication.swift
+++ Sources/JWTSecurityLab/webApp/WebApplication.swift
@@ -22,6 +22,7 @@ class WebApplication {
         server["/lab1"] = { request in
             
             let cookieName = "lab1_cookie"
+            let secret = "secret".data(using: .utf8)!
             // answer = eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJ1c2VyIjoiYWRtaW4ifQ.
             
             let page = Template(raw: Resource.getAppResource(relativePath: "templates/pageResponse.html"))
@@ -37,7 +38,7 @@ class WebApplication {
             
             if let token = request.cookies[cookieName] {
                 do {
-                    authorizedLogin = try JWTdecode(token, algorithm: .hs256("secret".data(using: .utf8)!)).claims["user"] as? String
+                    authorizedLogin = try JWTdecode(token, algorithm: .hs256(secret)).claims["user"] as? String
                 } catch {
                     // simulated bahaviour of CVE-2020-15957
                     do {
@@ -53,14 +54,87 @@ class WebApplication {
                 let password = (formData.first { $0.0 == "password" }.map { $0.1 }) {
                     if ["jim", "admin"].contains(login) {
                         if login == "jim", password == "12345" {
-                            let token = JWTencode(claims: ["user": login], algorithm: .hs256("secret".data(using: .utf8)!))
-                            headers.setCookie(name: cookieName, value: token + ";Max-Age=3000; HttpOnly, Secure")
+                            let token = JWTencode(claims: ["user": login], algorithm: .hs256(secret))
+                            headers.setCookie(name: cookieName, value: token)
                             authorizedLogin = login
                         } else {
-                            page.set(variables: ["message":"Invalid password for user <b>\(login)</b>."], inNest: "error")
+                            let errorMsg = String(format: "Invalid password for user %@.", Template.htmlNode(type: "b", content: login))
+                            page.set(variables: ["message":errorMsg], inNest: "error")
                         }
                     } else {
-                        page.set(variables: ["message":"User <b>\(login)</b> does't exist."], inNest: "error")
+                        let errorMsg = String(format: "User %@ does't exist.", Template.htmlNode(type: "b", content: login))
+                        page.set(variables: ["message":errorMsg], inNest: "error")
+                    }
+            } else if let _ = (formData.first { $0.0 == "logout" }.map { $0.1 }) {
+                headers.unsetCookie(name: cookieName)
+                page.set(variables: ["message":"Successfully signed out."], inNest: "success")
+                authorizedLogin = nil
+            }
+            
+            if let login = authorizedLogin, ["jim", "admin"].contains(login) {
+                loginTemplate.set(variables: ["path":request.path,"login":login], inNest: "authorized")
+                if login == "admin" {
+                    page.set(variables: ["message":"Job well done! Contratulations. You have taken over admin's account!"], inNest: "success")
+                } else {
+                    let msg = String(format: "Successfully logged as %@. Now your task is to breach the security and authenticate as admin.", Template.htmlNode(type: "b", content: login))
+                    page.set(variables: ["message":msg], inNest: "info")
+                }
+            } else {
+                loginTemplate.set(variables: ["path":request.path], inNest: "unauthorized")
+            }
+            
+            page.set(variables: ["body": loginTemplate.output()])
+            return page.asResponse(withHeaders: headers)
+        }
+        
+        server["/lab2"] = { request in
+            
+            let cookieName = "lab2_cookie"
+            let secret = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9".data(using: .utf8)!
+            
+            let page = Template(raw: Resource.getAppResource(relativePath: "templates/pageResponse.html"))
+            page.set(variables: ["title":"Lab 2: CVE-2019-7644"])
+            page.set(variables: ["url":"/css/login-form.css"], inNest: "css")
+            
+            let loginTemplate = Template(raw: Resource.getAppResource(relativePath: "templates/loginForm.html"))
+            let instructions = Template(raw: Resource.getAppResource(relativePath: "templates/lab2Instructions.html"))
+            loginTemplate.set(variables: ["instructions":instructions.output()])
+            
+            let headers = HttpHeaders()
+            var authorizedLogin: String?
+            
+            if let token = request.cookies[cookieName] {
+                do {
+                    authorizedLogin = try JWTdecode(token, algorithm: .hs256(secret)).claims["user"] as? String
+                } catch {
+                    // simulated bahaviour of CVE-2019-7644
+                    let tokenParts = token.split(".")
+                    if tokenParts.count == 3, let base = "\(tokenParts[0]).\(tokenParts[1])".data(using: .utf8) {
+                        let alg = Algorithm.hs256(secret)
+                        let signature = base64encode(alg.algorithm.sign(base))
+                        let errorMsg = String(format: "Invalid signature. Expected %@ got %@",
+                                              Template.htmlNode(type: "span", attributes: ["class":"fw-light"], content: signature),
+                                              Template.htmlNode(type: "span", attributes: ["class":"fw-light"], content: tokenParts[2]))
+                        page.set(variables: ["message":errorMsg], inNest: "error")
+                    }
+                }
+            }
+            
+            let formData = request.parseUrlencodedForm()
+            if let login = (formData.first { $0.0 == "login" }.map { $0.1 }),
+                let password = (formData.first { $0.0 == "password" }.map { $0.1 }) {
+                    if ["jim", "admin"].contains(login) {
+                        if login == "jim", password == "12345" {
+                            let token = JWTencode(claims: ["user": login], algorithm: .hs256(secret))
+                            headers.setCookie(name: cookieName, value: token)
+                            authorizedLogin = login
+                        } else {
+                            let errorMsg = String(format: "Invalid password for user %@.", Template.htmlNode(type: "b", content: login))
+                            page.set(variables: ["message":errorMsg], inNest: "error")
+                        }
+                    } else {
+                        let errorMsg = String(format: "User %@ does't exist.", Template.htmlNode(type: "b", content: login))
+                        page.set(variables: ["message":errorMsg], inNest: "error")
                     }
             } else if let _ = (formData.first { $0.0 == "logout" }.map { $0.1 }) {
                 headers.unsetCookie(name: cookieName)
@@ -73,7 +147,8 @@ class WebApplication {
                 if login == "admin" {
                     page.set(variables: ["message":"Job well done! Contratulations. You have taken over admin's account!"], inNest: "success")
                 } else {
-                    page.set(variables: ["message":"Successfully logged as \(login). Now your task is to breach the security and authenticate as admin."], inNest: "info")
+                    let msg = String(format: "Successfully logged as %@. Now your task is to breach the security and authenticate as admin.", Template.htmlNode(type: "b", content: login))
+                    page.set(variables: ["message":msg], inNest: "info")
                 }
             } else {
                 loginTemplate.set(variables: nil, inNest: "unauthorized")
